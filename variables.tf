68265342eafa24fe30a685a1f545cdc06a0514b8
Thanos <manoloudas88@gmail.com>
2024-05-19 20:14:28 +0300

    Intitial commit

---
+++ variables.tf
@@ -0,0 +1,30 @@
+##################################################################################
+# VARIABLES
+##################################################################################
+
+variable "region" {
+  type        = string
+  description = "(Optional) AWS Region to use. Default: us-east-1"
+  default     = "us-east-1"
+}
+
+variable "prefix" {
+  type        = string
+  description = "(Optional) Prefix to use for all resources in this module. Default: globo-dev"
+}
+
+variable "cidr_block" {
+  type        = string
+  description = "(Optional) The CIDR block for the VPC. Default:10.42.0.0/16"
+}
+
+variable "public_subnets" {
+  type        = map(string)
+  description = "(Optional) Map of public subnets to create with CIDR blocks. Key will be used as subnet name with prefix. Default: subnet-1 ="
+}
+
+
+variable "billing_code" {
+  type        = string
+  description = "(Required) Billing code for network resources"
+}
